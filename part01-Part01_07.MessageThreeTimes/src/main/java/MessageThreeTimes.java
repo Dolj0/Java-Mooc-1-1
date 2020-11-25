cf7d4be07ffdbee376766f8390767c75beffb078
Joel <74183085+Dolj0@users.noreply.github.com>
2020-11-25 17:56:56 +0000

    Add files via upload

---
+++ part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        // Write your program here
+        
+        String I = scanner.nextLine();
+        System.out.println(I);
+        System.out.println(I);
+        System.out.println(I);
+    }
+}
